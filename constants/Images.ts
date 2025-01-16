493c2572a1b0697c6ac8742bfe22a9e936084e42
RakshitRabugotra <rakshit.rabugotra000@gmail.com>
2025-01-17 01:39:56 +0530

    build: add image assets

---
+++ constants/Images.ts
@@ -0,0 +1,6 @@
+export default {
+  call: require('@/assets/icons/48/call.png'),
+  phone: require('@/assets/icons/64/phone.png'),
+  location: require('@/assets/icons/64/location.png'),
+  chatBubble: require('@/assets/icons/64/chat-bubble.png')
+}
