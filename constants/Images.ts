b81121948cb2d92931e551818df0e4a0cdd6e89a
RakshitRabugotra <rakshit.rabugotra000@gmail.com>
2025-01-22 21:09:39 +0530

    build: add icon static asset

--- constants/Images.ts
+++ constants/Images.ts
@@ -1,7 +1,15 @@
 export default {
   call: require('@/assets/icons/48/call.png'),
+  route: require('@/assets/icons/64/route.png'),
   phone: require('@/assets/icons/64/phone.png'),
   location: require('@/assets/icons/64/location.png'),
   siren: require('@/assets/icons/64/siren.png'),
-  chatBubble: require('@/assets/icons/64/chat-bubble.png')
+  chatBubble: require('@/assets/icons/64/chat-bubble.png'),
+  // The tabs in the helpline
+  floater: require('@/assets/icons/64/floater.png'),
+  emergency: require('@/assets/icons/64/emergency-bell.png'),
+  ambulance: require('@/assets/icons/64/ambulance.png'),
+  shield: require('@/assets/icons/64/shield.png'),
+  cloudAlert: require('@/assets/icons/64/cloud-alert.png'),
+  warningTriangle: require('@/assets/icons/64/warning-triangle.png'),
 }
